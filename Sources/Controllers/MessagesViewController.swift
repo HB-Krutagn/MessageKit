7578bc0560461c6ce40783ffe6a4a977e203521d
HB-Krutagn <44233824+HB-Krutagn@users.noreply.github.com>
2022-09-07 09:57:06 +0530

    Commented import InputBarAccessoryView

--- Sources/Controllers/MessagesViewController.swift
+++ Sources/Controllers/MessagesViewController.swift
@@ -22,7 +22,7 @@
 
 import Combine
 import Foundation
-import InputBarAccessoryView
+// import InputBarAccessoryView
 import UIKit
 
 /// A subclass of `UIViewController` with a `MessagesCollectionView` object
