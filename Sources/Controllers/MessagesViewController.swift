2a0209d83b5f4d7b71d47bab2b049e85c20b5324
HB-Krutagn <44233824+HB-Krutagn@users.noreply.github.com>
2022-09-09 10:30:07 +0530

    Update MessagesViewController.swift

--- Sources/Controllers/MessagesViewController.swift
+++ Sources/Controllers/MessagesViewController.swift
@@ -173,10 +173,14 @@ open class MessagesViewController: UIViewController, UICollectionViewDelegateFlo
       cell.configure(with: message, at: indexPath, and: messagesCollectionView)
       return cell
     case .document:
-      let cell = messagesCollectionView.dequeueReusableCell(DocumentMessageCell.self, for: indexPath)
+      if let cell = messagesDataSource.documentCell(for: message, at: indexPath, in: messagesCollectionView) {
+        return cell
+      } else {
+        let cell = messagesCollectionView.dequeueReusableCell(DocumentMessageCell.self, for: indexPath)
 //       cell.indexPath = indexPath
-      cell.configure(with: message, at: indexPath, and: messagesCollectionView)
-      return cell
+        cell.configure(with: message, at: indexPath, and: messagesCollectionView)
+        return cell
+      }
 //     case .reply:
 //       let cell = messagesCollectionView.dequeueReusableCell(ReplyMessageCell.self, for: indexPath)
 //       cell.indexPath = indexPath
