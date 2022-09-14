ef5e4f450cb3b7bc40923e4d311d0772bb171fdc
HB-Krutagn <44233824+HB-Krutagn@users.noreply.github.com>
2022-09-14 10:26:06 +0530

    Update MediaMessageCell.swift

--- Sources/Views/Cells/MediaMessageCell.swift
+++ Sources/Views/Cells/MediaMessageCell.swift
@@ -24,84 +24,96 @@ import UIKit
 
 /// A subclass of `MessageContentCell` used to display video and audio messages.
 open class MediaMessageCell: MessageContentCell {
-  /// The play button view to display on video messages.
-  open lazy var playButtonView: PlayButtonView = {
-    let playButtonView = PlayButtonView()
-    return playButtonView
-  }()
-
-  /// The image view display the media content.
-  open var imageView: UIImageView = {
-    let imageView = UIImageView()
-    imageView.contentMode = .scaleAspectFill
-    return imageView
-  }()
+    /// The play button view to display on video messages.
+    open lazy var playButtonView: PlayButtonView = {
+        let playButtonView = PlayButtonView()
+        return playButtonView
+    }()
     
-  open var messageProgressView: HBProgressView = {
-    let progressView = HBProgressView()
-    progressView.clipsToBounds = true
-    progressView.layer.masksToBounds = true
-    return progressView
-   }()
-
-
-  // MARK: - Methods
-
-  /// Responsible for setting up the constraints of the cell's subviews.
-  open func setupConstraints() {
-    imageView.fillSuperview()
-    playButtonView.centerInSuperview()
-    playButtonView.constraint(equalTo: CGSize(width: 35, height: 35))
-    messageProgressView.fillSuperview()
-  }
-
-  open override func setupSubviews() {
-    super.setupSubviews()
-    messageContainerView.addSubview(imageView)
-    messageContainerView.addSubview(playButtonView)
-    messageContainerView.addSubview(messageProgressView)
-    setupConstraints()
-  }
-
-  open override func prepareForReuse() {
-    super.prepareForReuse()
-    imageView.image = nil
-  }
-
-  open override func configure(
-    with message: MessageType,
-    at indexPath: IndexPath,
-    and messagesCollectionView: MessagesCollectionView)
-  {
-    super.configure(with: message, at: indexPath, and: messagesCollectionView)
-
-    guard let displayDelegate = messagesCollectionView.messagesDisplayDelegate else {
-      fatalError(MessageKitError.nilMessagesDisplayDelegate)
+    /// The image view display the media content.
+    open var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    open var messageProgressView: HBProgressView = {
+        let progressView = HBProgressView()
+        progressView.clipsToBounds = true
+        progressView.layer.masksToBounds = true
+        return progressView
+    }()
+    
+    // MARK: - Methods
+    
+    /// Responsible for setting up the constraints of the cell's subviews.
+    open func setupConstraints() {
+        imageView.fillSuperview()
+        playButtonView.centerInSuperview()
+        playButtonView.constraint(equalTo: CGSize(width: 35, height: 35))
+        messageProgressView.fillSuperview()
     }
-
-    switch message.kind {
-    case .photo(let mediaItem):
-      imageView.image = mediaItem.image ?? mediaItem.placeholderImage
-      playButtonView.isHidden = true
-    case .video(let mediaItem):
-      imageView.image = mediaItem.image ?? mediaItem.placeholderImage
-      playButtonView.isHidden = false
-    default:
-      break
+    
+    open override func setupSubviews() {
+        super.setupSubviews()
+        messageContainerView.addSubview(imageView)
+        messageContainerView.addSubview(playButtonView)
+        messageContainerView.addSubview(messageProgressView)
+        setupConstraints()
     }
-
-    displayDelegate.configureMediaMessageImageView(imageView, for: message, at: indexPath, in: messagesCollectionView)
-  }
-
-  /// Handle tap gesture on contentView and its subviews.
-  open override func handleTapGesture(_ gesture: UIGestureRecognizer) {
-    let touchLocation = gesture.location(in: imageView)
-
-    guard imageView.frame.contains(touchLocation) else {
-      super.handleTapGesture(gesture)
-      return
+    
+    open override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
     }
-    delegate?.didTapImage(in: self)
-  }
+    
+    open override func configure(
+        with message: MessageType,
+        at indexPath: IndexPath,
+        and messagesCollectionView: MessagesCollectionView) {
+        super.configure(with: message, at: indexPath, and: messagesCollectionView)
+        
+        guard let displayDelegate = messagesCollectionView.messagesDisplayDelegate else {
+            fatalError(MessageKitError.nilMessagesDisplayDelegate)
+        }
+        
+        switch message.kind {
+        case .photo(let mediaItem):
+            imageView.image = mediaItem.image ?? mediaItem.placeholderImage
+            playButtonView.isHidden = true
+        case .video(let mediaItem):
+            imageView.image = mediaItem.image ?? mediaItem.placeholderImage
+            playButtonView.isHidden = false
+        default:
+            break
+        }
+        
+        displayDelegate.configureMediaMessageImageView(imageView, for: message, at: indexPath, in: messagesCollectionView)
+    }
+    
+    /// Handle tap gesture on contentView and its subviews.
+    open override func handleTapGesture(_ gesture: UIGestureRecognizer) {
+        if !messageProgressView.isHidden {
+            let touchLocation = gesture.location(in: messageProgressView)
+            if messageProgressView.btnCancel.frame.contains(touchLocation) {
+                delegateProgress?.didTapCancelProgress(in: self)
+            } else if messageProgressView.btnRetry.frame.contains(touchLocation) {
+                delegateProgress?.didTapRetryProgress(in: self)
+            } else {
+                let touchLocation = gesture.location(in: imageView)
+                guard imageView.frame.contains(touchLocation) else {
+                    super.handleTapGesture(gesture)
+                    return
+                }
+                delegate?.didTapImage(in: self)
+            }
+        } else {
+            let touchLocation = gesture.location(in: imageView)
+            guard imageView.frame.contains(touchLocation) else {
+                super.handleTapGesture(gesture)
+                return
+            }
+            delegate?.didTapImage(in: self)
+        }
+    }
 }
-
