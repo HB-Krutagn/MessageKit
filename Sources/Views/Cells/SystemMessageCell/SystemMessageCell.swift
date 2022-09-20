8ddb917fc33b08b787fd5cf65709b00b076a8c2f
HB-Krutagn <44233824+HB-Krutagn@users.noreply.github.com>
2022-09-20 13:58:00 +0530

    Add files via upload

---
+++ Sources/Views/Cells/SystemMessageCell/SystemMessageCell.swift
@@ -0,0 +1,36 @@
+//
+//  SystemMessageCell.swift
+//  qtconnect
+//
+//  Created by hb on 17/08/22.
+//
+
+import UIKit
+import MessageKit
+
+open class SystemMessageCell: UICollectionViewCell {
+    @IBOutlet weak var lblMessage: PaddingLabel!
+    
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        lblMessage.layer.cornerRadius = 6
+        lblMessage.clipsToBounds = true
+    }
+    
+    open func configure(with message: MessageType, at _: IndexPath, and _: MessagesCollectionView) {
+        // Do stuff
+        switch message.kind {
+        case .custom(let data):
+            if let systemMessage = data as? String {
+                lblMessage.text = systemMessage
+            } else if let systemMessage = data as? NSAttributedString {
+                lblMessage.attributedText = systemMessage
+            } else {
+                return
+            }
+        default:
+            break
+        }
+    }
+}
