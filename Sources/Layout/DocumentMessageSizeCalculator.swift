1bf3d5dab2cccfd61515a5801fd706b3404fd493
HB-Dimple <hb.dimple@hiddenbrains.in>
2023-07-26 14:00:07 +0530

    Update DocumentMessageSizeCalculator.swift

--- Sources/Layout/DocumentMessageSizeCalculator.swift
+++ Sources/Layout/DocumentMessageSizeCalculator.swift
@@ -16,12 +16,31 @@ open class DocumentMessageSizeCalculator: MessageSizeCalculator {
             let maxWidth = messageContainerMaxWidth(for: message, at: indexPath)
             if maxWidth < item.size.width {
                 // Maintain the ratio if width is too great
-                let height = maxWidth * item.size.height / item.size.width
-                return CGSize(width: maxWidth, height: height)
+               var height = maxWidth * item.size.height / item.size.width
+              
+                let font = UIFont.systemFont(ofSize: 18.0, weight: .regular)
+                 var height1 = self.heightForView(text: (item.text ?? ""), font: font, width: item.size.width) + 10.0
+                if item.text == "" {
+                    height1 = 0
+                }
+                
+                return CGSize(width: maxWidth, height: height + height1 )
             }
-            return CGSize(width: item.size.width, height: item.size.height)
+            let font = UIFont.systemFont(ofSize: 18.0, weight: .regular)
+             var height1 = self.heightForView(text: (item.text ?? ""), font: font, width: item.size.width)
+            if item.text == "" {
+                height1 = 0
+            }
+            
+            return CGSize(width: item.size.width, height: item.size.height + height1)
         default:
             fatalError("messageContainerSize received unhandled MessageDataType: \(message.kind)")
         }
+    }
+    func heightForView(text: String, font: UIFont, width: CGFloat) -> CGFloat {
+        let attributedText = NSAttributedString(string: text, attributes: [.font: font])
+        let framesetter = CTFramesetterCreateWithAttributedString(attributedText)
+        let size = CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRange(location: 0,length: 0), nil, CGSize(width: width, height: .greatestFiniteMagnitude), nil)
+        return size.height
     }
 }
